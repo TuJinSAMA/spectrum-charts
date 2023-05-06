6020345445011e73b1774f02283839b05420b227
gongyuqi <gong_yu_qi@163.com>
2023-05-06 18:36:47 +0800

    feat: progressBar opt 50%

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -9,11 +9,7 @@ const ColorMap = require('colormap')
 const ProgressBar = React.forwardRef(
   ({ maxDb = 0, minDb = -140, percentage = 0 }, ref) => {
     const progressBox = useRef(null)
-    // let boxWidth = 0
-    // let boxHeight = 0
-    // let canvasCtx = null
-    // let fallsCanvasCtx = null
-    // let colors = null
+
     let [state, setState] = useState({
       baseData: [],
       boxWidth: 0,
@@ -24,73 +20,73 @@ const ProgressBar = React.forwardRef(
     })
 
     useEffect(() => {
-      if (progressBox.current.clientWidth !== 0) {
-        initComponent()
-      } else {
-        nextTick(() => {
-          initComponent()
-        })
-      }
+      initComponent()
     }, [])
 
+    // 初始化组件
     const initComponent = () => {
-      // boxWidth = progressBox.current.clientWidth
-      // boxHeight = progressBox.current.clientHeight
-      setState({
-        ...state,
-        boxWidth: progressBox.current.clientWidth,
-        boxHeight: progressBox.current.clientHeight,
-      })
-      createCanvas()
-      createFallsCanvas()
-      initColors()
+      // 先获取进度条容器的宽度和高度
+      const boxWidth = progressBox.current.clientWidth
+      const boxHeight = progressBox.current.clientHeight
+      // 创建进度条画布
+      const [canvasCtx, fallsCanvasCtx] = createCanvas(boxWidth, boxHeight)
+      // 初始化颜色图
+      const colors = initColors()
+      // 更新到 state 中
+      setState(s => ({
+        ...s,
+        boxWidth,
+        boxHeight,
+        canvasCtx,
+        fallsCanvasCtx,
+        colors,
+      }))
     }
 
-    const createCanvas = () => {
+    // 创建画布
+    // 这里需要创建两个画布，一个用来绘制瀑布图，另一个将绘制好的瀑布图展示在页面上
+    const createCanvas = (width, height) => {
+      // 创建用来绘制的画布
+      const fallsCanvas = document.createElement('canvas')
+      fallsCanvas.width = width
+      fallsCanvas.height = height
+      const fallsCanvasCtx = fallsCanvas.getContext('2d')
+
+      // 创建最终展示的画布
       const canvas = document.createElement('canvas')
       canvas.className = 'progress_canvas'
-      canvas.width = state.boxWidth
-      canvas.height = state.boxHeight
+      canvas.width = width
+      canvas.height = height
+      // 将最终展示的画布添加到容器里
       progressBox.current.appendChild(canvas)
-      setState({
-        ...state,
-        canvasCtx: canvas.getContext('2d'),
-      })
-      console.log(state);
-    }
-    const createFallsCanvas = () => {
-      const canvas = document.createElement('canvas')
-      canvas.width = state.boxWidth
-      canvas.height = state.boxHeight
-      setState({
-        ...state,
-        fallsCanvasCtx: canvas.getContext('2d'),
-      })
+      const canvasCtx = canvas.getContext('2d')
+      return [canvasCtx, fallsCanvasCtx]
     }
 
+    // 初始化颜色图
     const initColors = () => {
       if (maxDb === undefined || minDb === undefined) return
       const len = maxDb - minDb
-      let colors = ColorMap({
+      return ColorMap({
         colormap: 'jet',
         nshades: len,
         format: 'rba',
         alpha: 1,
       })
-      setState({
-        ...state,
-        colors,
-      })
     }
 
+    // 绘制进度条
+    // 会将该方法暴露给父组件 父组件调用时传入进度条数据
     const drawProgress = data => {
-      setState({
-        ...state,
-        baseData: data,
-      })
+      console.log(data);
+      // setState(s => ({
+      //   ...state,
+      //   baseData: data,
+      // }))
+      // 数据聚合
       let len = data.length
-      console.log(state.boxWidth)
       const scale = len / state.boxWidth
+      console.log(scale)
       let arr = []
       for (let i = len; i > 0; i -= scale) {
         const startIndex = round(i - scale)
@@ -106,7 +102,6 @@ const ProgressBar = React.forwardRef(
             }
             newCol.push(result / cols.length)
           })
-          console.log(state)
           drawColImgData(newCol)
           arr.push(newCol)
         } else {
@@ -114,19 +109,74 @@ const ProgressBar = React.forwardRef(
         }
         if (round(i - scale) === 0) break
       }
-      canvasCtx.value.drawImage(
-        fallsCanvasCtx.value.canvas,
+      console.log(arr)
+      state.canvasCtx.drawImage(
+        state.fallsCanvasCtx.canvas,
         0,
         0,
-        boxWidth.value,
-        boxHeight.value,
+        state.boxWidth,
+        state.boxHeight,
         0,
         0,
-        boxWidth.value,
-        boxHeight.value
+        state.boxWidth,
+        state.boxHeight
       )
-      drawLineBox()
-      drawSelectionBox()
+      // drawLineBox()
+      // drawSelectionBox()
+    }
+
+    const drawColImgData = data => {
+      const imageData = state.fallsCanvasCtx.createImageData(1, state.boxHeight)
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        const cIndex = getCurrentColor(data[i / 4])
+        const color = state.colors[cIndex]
+        imageData.data[i + 0] = color[0]
+        imageData.data[i + 1] = color[1]
+        imageData.data[i + 2] = color[2]
+        imageData.data[i + 3] = 255
+      }
+      state.fallsCanvasCtx.putImageData(imageData, 0, 0)
+      // 将已生成的图像向右移动一个像素
+      state.fallsCanvasCtx.drawImage(
+        state.fallsCanvasCtx.canvas,
+        0,
+        0,
+        state.boxWidth,
+        state.boxHeight,
+        1,
+        0,
+        state.boxWidth,
+        state.boxHeight
+      )
+      return data
+    }
+
+    const disposeColData = data => {
+      let len = data.length
+      const scale = len / state.boxHeight
+      let result = []
+      for (let i = 0; i <= len; i += scale) {
+        const startIndex = round(i)
+        const endIndex = round(i + scale)
+        let points = data.slice(startIndex, endIndex)
+        let point =
+          points.reduce((res, item) => (res += item), 0) / points.length
+        result.push(point)
+      }
+      return result
+    }
+
+    // 返回数据对应的 color 集合
+    const getCurrentColor = value => {
+      const min = 0
+      const max = state.colors.length - 1
+      if (value <= minDb) {
+        return min
+      } else if (value >= maxDb) {
+        return max
+      } else {
+        return round(((value - minDb) / (maxDb - minDb)) * max)
+      }
     }
 
     const lineBox = null
@@ -211,26 +261,26 @@ const ProgressBar = React.forwardRef(
       }
     }
 
-    const handlerMoveSelection = e => {
-      if (!isSelection.value) return
-      isClick.value = false
-      selectionBox.value.style.opacity = 1
-      endX.value =
-        e.target.className === 'selection_box'
-          ? selectionBox.value.offsetLeft + e.offsetX
-          : e.target.className === 'line_box'
-          ? lineBox.value.offsetLeft + e.offsetX
-          : e.offsetX
-      if (endX.value > beginX.value) {
-        selectionBox.value.style.left = `${beginX.value}px`
-        selectionBox.value.style.width = `${endX.value - beginX.value}px`
-      } else if (endX.value < beginX.value) {
-        selectionBox.value.style.left = `${endX.value}px`
-        selectionBox.value.style.width = `${Math.abs(
-          endX.value - beginX.value
-        )}px`
-      }
-    }
+    // const handlerMoveSelection = e => {
+    //   if (!isSelection.value) return
+    //   isClick.value = false
+    //   selectionBox.value.style.opacity = 1
+    //   endX.value =
+    //     e.target.className === 'selection_box'
+    //       ? selectionBox.value.offsetLeft + e.offsetX
+    //       : e.target.className === 'line_box'
+    //       ? lineBox.value.offsetLeft + e.offsetX
+    //       : e.offsetX
+    //   if (endX.value > beginX.value) {
+    //     selectionBox.value.style.left = `${beginX.value}px`
+    //     selectionBox.value.style.width = `${endX.value - beginX.value}px`
+    //   } else if (endX.value < beginX.value) {
+    //     selectionBox.value.style.left = `${endX.value}px`
+    //     selectionBox.value.style.width = `${Math.abs(
+    //       endX.value - beginX.value
+    //     )}px`
+    //   }
+    // }
 
     const throttleMouseMove = throttle(handlerMouseMove, 4)
 
@@ -254,25 +304,25 @@ const ProgressBar = React.forwardRef(
       emit('progress-change', percent)
     }
 
-    const handlerSelectionUp = e => {
-      if (!isSelection.value) return
-      isSelection.value = false
-      if (totalCount.value === 0 || isClick.value) return
-      let start, end
-      if (beginX.value > endX.value) {
-        start = Math.round((endX.value / boxWidth.value) * totalCount.value)
-        end = Math.round((beginX.value / boxWidth.value) * totalCount.value)
-      } else {
-        start = Math.round((beginX.value / boxWidth.value) * totalCount.value)
-        end = Math.round((endX.value / boxWidth.value) * totalCount.value)
-      }
-      emit('progress-selected', {
-        start,
-        end,
-      })
-    }
+    // const handlerSelectionUp = e => {
+    //   if (!isSelection.value) return
+    //   isSelection.value = false
+    //   if (totalCount.value === 0 || isClick.value) return
+    //   let start, end
+    //   if (beginX.value > endX.value) {
+    //     start = Math.round((endX.value / boxWidth.value) * totalCount.value)
+    //     end = Math.round((beginX.value / boxWidth.value) * totalCount.value)
+    //   } else {
+    //     start = Math.round((beginX.value / boxWidth.value) * totalCount.value)
+    //     end = Math.round((endX.value / boxWidth.value) * totalCount.value)
+    //   }
+    //   emit('progress-selected', {
+    //     start,
+    //     end,
+    //   })
+    // }
 
-    const percentStorage = 0
+    // const percentStorage = 0
     // watch(props.percentage, newValue => {
     //   if (isMove.value) return
     //   percentStorage.value = newValue
@@ -285,72 +335,16 @@ const ProgressBar = React.forwardRef(
       console.log('percentage changed!', percentage)
     }, [percentage])
 
-    const totalCount = 0
+    // const totalCount = 0
 
-    const setLineBoxStyle = data => {
-      const percent =
-        (data.aggregation_frame * (data.fft_size - 0)) / totalCount.value
-      const lineWidth = percent * boxWidth.value
-      if (lineBox.value) {
-        lineBox.value.style.width = lineWidth + 'px'
-      }
-    }
-
-    const drawColImgData = data => {
-      const imageData = state.fallsCanvasCtx.createImageData(1, boxHeight.value)
-      for (let i = 0; i < imageData.data.length; i += 4) {
-        const cIndex = getCurrentColor(data[i / 4])
-        const color = state.colors[cIndex]
-        imageData.data[i + 0] = color[0]
-        imageData.data[i + 1] = color[1]
-        imageData.data[i + 2] = color[2]
-        imageData.data[i + 3] = 255
-      }
-      state.fallsCanvasCtx.putImageData(imageData, 0, 0)
-      // 将已生成的图像向右移动一个像素
-      state.fallsCanvasCtx.drawImage(
-        state.fallsCanvasCtx.value.canvas,
-        0,
-        0,
-        boxWidth.value,
-        boxHeight.value,
-        1,
-        0,
-        boxWidth.value,
-        boxHeight.value
-      )
-      return data
-    }
-
-    const disposeColData = data => {
-      let len = data.length
-      const scale = len / state.boxHeight
-      let result = []
-      for (let i = 0; i <= len; i += scale) {
-        const startIndex = round(i)
-        const endIndex = round(i + scale)
-        let points = data.slice(startIndex, endIndex)
-        let point =
-          points.reduce((res, item) => (res += item), 0) / points.length
-        result.push(point)
-      }
-      return result
-    }
-
-    // 返回数据对应的 color 集合
-    const getCurrentColor = value => {
-      const min = 0
-      const max = colors.value.length - 1
-      if (value <= props.minDb) {
-        return min
-      } else if (value >= props.maxDb) {
-        return max
-      } else {
-        return round(
-          ((value - props.minDb) / (props.maxDb - props.minDb)) * max
-        )
-      }
-    }
+    // const setLineBoxStyle = data => {
+    //   const percent =
+    //     (data.aggregation_frame * (data.fft_size - 0)) / totalCount.value
+    //   const lineWidth = percent * boxWidth.value
+    //   if (lineBox.value) {
+    //     lineBox.value.style.width = lineWidth + 'px'
+    //   }
+    // }
 
     const resetProgress = () => {
       canvasCtx.value.clearRect(0, 0, boxWidth.value, boxHeight.value)
@@ -359,30 +353,10 @@ const ProgressBar = React.forwardRef(
       baseData.value = []
     }
 
-    const setTotalCount = count => {
-      if (count === totalCount.value) return
-      totalCount.value = count
-    }
-
-    const setProgressWidth = () => {
-      boxWidth.value = progressBox.current.clientWidth
-      boxHeight.value = progressBox.current.clientHeight
-      canvasCtx.value.canvas.width = boxWidth.value
-      canvasCtx.value.canvas.height = boxHeight.value
-      fallsCanvasCtx.value.canvas.width = boxWidth.value
-      fallsCanvasCtx.value.canvas.height = boxHeight.value
-      if (lineBox.value) {
-        lineBox.value.remove()
-        baseData.value.length > 0 && drawProgress(baseData.value)
-        lineBox.value.style.left = `${percentStorage.value}%`
-      }
-    }
-
-    const resetSelectionBox = () => {
-      if (!selectionBox.value) return
-      selectionBox.value.style.width = 0
-      selectionBox.value.style.opacity = 0
-    }
+    // const setTotalCount = count => {
+    //   if (count === totalCount.value) return
+    //   totalCount.value = count
+    // }
 
     useImperativeHandle(ref, () => ({
       drawProgress: drawProgress,
